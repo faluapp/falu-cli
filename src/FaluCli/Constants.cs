6a56513ffcb3a4118cfcd3e3281a2052d37bc179
Maxwell Weru <mburumaxwell@gmail.com>
2022-09-12 13:32:34 +0300

    Added regex for ISO8601 duration

--- src/FaluCli/Constants.cs
+++ src/FaluCli/Constants.cs
@@ -31,4 +31,5 @@ internal class Constants
     public static readonly Regex MessageTemplateIdFormat = new(@"^(?:mtpl|tmpl)_[a-zA-Z0-9]{20,30}$");
     public static readonly Regex MessageTemplateAliasFormat = new(@"^[a-zA-Z]([a-zA-Z0-9\-_]+)$");
     public static readonly Regex E164PhoneNumberFormat = new(@"^\+[1-9]\d{1,14}$"); // https://ihateregex.io/expr/e164-phone/
+    public static readonly Regex Iso8061DurationFormat = new(@"^P(([0-9.,]+Y)?([0-9.,]+M)?([0-9.,]+W)?([0-9.,]+D)?)(T([0-9.,]+H)?([0-9.,]+M)?([0-9.,]+S)?)?$"); // https://gist.github.com/tristanls/356ce5aea0054b770d49
 }
