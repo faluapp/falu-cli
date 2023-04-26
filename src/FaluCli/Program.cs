a7f3b1a095ea842ce8f7e78eafb8d9450160f8a9
Maxwell Weru <mburumaxwell@gmail.com>
2023-04-26 11:37:28 +0300

    Rollback to net6.0 because of trimming issues

--- src/FaluCli/Program.cs
+++ src/FaluCli/Program.cs
@@ -82,7 +82,7 @@ var builder = new CommandLineBuilder(rootCommand)
                 ["Logging:LogLevel:Default"] = "Information",
                 ["Logging:LogLevel:Microsoft"] = "Warning",
 
-                // See https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-7.0#logging
+                // See https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-6.0#logging
                 ["Logging:LogLevel:System.Net.Http.HttpClient"] = "None", // removes all we do not need
                 ["Logging:LogLevel:System.Net.Http.HttpClient.Oidc.ClientHandler"] = verbose ? "Trace" : "Warning", // add what we need
                 ["Logging:LogLevel:System.Net.Http.HttpClient.FaluCliClient.ClientHandler"] = verbose ? "Trace" : "Warning", // add what we need
