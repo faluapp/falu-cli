69d0fbd709b4009caa138b830eb8634dd5111fe9
Maxwell Weru <mburumaxwell@gmail.com>
2023-07-07 11:42:51 +0300

    Standardize serialized names for config values

--- src/FaluCli/Config/ConfigValues.cs
+++ src/FaluCli/Config/ConfigValues.cs
@@ -5,20 +5,34 @@ namespace Falu.Config;
 
 internal record ConfigValues
 {
-    [JsonPropertyName("skip-update-checks")]
+    [JsonPropertyName("skip_update_checks")]
     public bool SkipUpdateChecks { get; set; }
 
+    [JsonPropertyName("retries")]
     public int Retries { get; set; } = 0;
+
+    [JsonPropertyName("timeout")]
     public int Timeout { get; set; } = 120;
+
+    [JsonPropertyName("default_workspace_id")]
     public string? DefaultWorkspaceId { get; set; }
+
+    [JsonPropertyName("default_live_mode")]
     public bool DefaultLiveMode { get; set; }
+
+    [JsonPropertyName("authentication")]
     public AuthenticationTokenConfigData? Authentication { get; set; }
 }
 
 internal record AuthenticationTokenConfigData
 {
+    [JsonPropertyName("access_token")]
     public string? AccessToken { get; set; }
+
+    [JsonPropertyName("access_token_expiry")]
     public DateTimeOffset? AccessTokenExpiry { get; set; }
+
+    [JsonPropertyName("refresh_token")]
     public string? RefreshToken { get; set; }
 
     [MemberNotNullWhen(true, nameof(AccessToken))]
