5c01d413c4c7171e6e9a8cd9909ebdff4cf5c6c2
Maxwell Weru <mburumaxwell@gmail.com>
2023-07-07 13:29:56 +0300

    Forwarding received events to a provided endpoint

--- src/FaluCli/Commands/Events/EventsListenCommand.cs
+++ src/FaluCli/Commands/Events/EventsListenCommand.cs
@@ -6,5 +6,15 @@ internal class EventsListenCommand : Command
     {
         this.AddOption<string[]>(new[] { "--event-type", "--type", "-t", },
                                  description: "The event to listen for. When not provided all events are listened to.");
+
+        this.AddOption<Uri>(new[] { "--forward-to", "-f", },
+                            description: "The URL that webhook events will be forwarded to.");
+
+        this.AddOption(new[] { "--skip-validation", },
+                       description: "Whether to skip certificate verification when forwarding to a HTTPS endpoint.",
+                       defaultValue: false);
+
+        this.AddOption<string>(new[] { "--webhook-secret", "--secret", },
+                               description: "The webhook secret to use. You can generate a random one or use one from a webhook endpoint if you are testing signature validation.");
     }
 }
