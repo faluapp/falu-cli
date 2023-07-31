f313e14729e812ebd14a1c17dee4cd8b0cfc3eef
Maxwell Weru <mburumaxwell@gmail.com>
2023-07-31 10:45:54 +0300

    Update code to use inbuilt ClientWebSocket type

--- src/FaluCli/Websockets/WebsocketHandler.cs
+++ src/FaluCli/Websockets/WebsocketHandler.cs
@@ -1,7 +1,6 @@
 ﻿using Falu.Client.Realtime;
 using System.Net.WebSockets;
 using System.Text.Json;
-using Websocket.Client;
 using SC = Falu.FaluCliJsonSerializerContext;
 
 namespace Falu.Websockets;
@@ -10,7 +9,7 @@ internal class WebsocketHandler : IDisposable
 {
     private readonly ILogger logger;
 
-    private WebsocketClient? client;
+    private ClientWebSocket? socket;
 
     public WebsocketHandler(ILogger<WebsocketHandler> logger)
     {
@@ -28,71 +27,88 @@ internal class WebsocketHandler : IDisposable
         logger.LogInformation("Connection valid for {Minutes} minutes", Convert.ToInt32((negotiation.Expires - DateTimeOffset.UtcNow).TotalMinutes));
         logger.LogDebug("Connection token:\r\n{Token}", token);
 
-        ClientWebSocket factory()
-        {
-            var inner = new ClientWebSocket();
-            inner.Options.AddSubProtocol("json.devproxy.falu.v1");
-            inner.Options.SetRequestHeader("Authorization", $"Bearer {token}");
-            return inner;
-        }
-
-        // create client
-        client = new WebsocketClient(url, factory)
-        {
-            ReconnectTimeout = null, // disable auto disconnect and reconnect becase we want to stay online even when no data comes in
-        };
-
-        // subscribe to incoming messages
-        client.MessageReceived.Subscribe(rm =>
-        {
-            // when the server closes the connection, cancel the token
-            var type = rm.MessageType;
-            if (type is WebSocketMessageType.Close)
-            {
-                logger.LogInformation("Server closed the websocket");
-                cancellationTokenSource.Cancel();
-            }
-
-            // create BinaryData for ease of manipulation and logging
-            var data = type switch
-            {
-                WebSocketMessageType.Binary => BinaryData.FromBytes(rm.Binary),
-                WebSocketMessageType.Text => BinaryData.FromString(rm.Text),
-                _ => throw new InvalidOperationException($"Unknown message type '{nameof(WebSocketMessageType)}.{type}'")
-            };
-            logger.LogDebug("Received message: {Data}", data);
-
-            // decode the message from JSON and send to handler
-            var message = JsonSerializer.Deserialize(data, SC.Default.RealtimeConnectionIncomingMessage) ?? throw new InvalidOperationException("Unable to desrialize incoming message");
-            handler(message, cancellationToken);
-        });
+        // create client socket
+        socket = new ClientWebSocket();
+        socket.Options.AddSubProtocol("json.devproxy.falu.v1");
+        socket.Options.SetRequestHeader("Authorization", $"Bearer {token}");
 
         // connect
-        await client.StartOrFail();
+        await socket.ConnectAsync(url, cancellationToken);
         logger.LogInformation("Connected to websocket server.");
+
+        // run without blocking the caller
+        _ = RunAsync(socket, handler, cancellationToken);
     }
 
     public Task SendMessageAsync(RealtimeConnectionOutgoingMessage message, CancellationToken cancellationToken = default)
     {
-        var client = GetClient();
-        var json = JsonSerializer.Serialize(message, SC.Default.RealtimeConnectionOutgoingMessage);
-        logger.LogDebug("Sending message: {Data}", json);
-        client.Send(json);
-        return Task.CompletedTask;
+        var socket = GetSocket();
+        var json = JsonSerializer.SerializeToUtf8Bytes(message, SC.Default.RealtimeConnectionOutgoingMessage);
+        if (logger.IsEnabled(LogLevel.Debug))
+        {
+            logger.LogDebug("Sending message: {Data}", System.Text.Encoding.UTF8.GetString(json));
+        }
+
+        return socket.SendAsync(json, WebSocketMessageType.Binary, endOfMessage: true, cancellationToken);
+    }
+
+    private async Task RunAsync(ClientWebSocket socket, Func<RealtimeConnectionIncomingMessage, CancellationToken, Task> handler, CancellationToken cancellationToken)
+    {
+        // listen to incoming messages
+        var buffer = new byte[1024];
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            // receive data from the socket
+            var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
+
+            // check for closure
+            if (result.CloseStatus is not null)
+            {
+                var status = result.CloseStatus.Value;
+                var description = result.CloseStatusDescription;
+                logger.LogDebug("WebSocket Connection closed. Status: {CloseStatus}, Description: {CloseStatusDescription}", status, description);
+                await socket.CloseAsync(status, description, cancellationToken);
+                return;
+            }
+
+            // check for close message
+            if (result.MessageType is WebSocketMessageType.Close)
+            {
+                logger.LogDebug("WebSocket Connection sent a close message.");
+                await socket.CloseAsync(WebSocketCloseStatus.Empty, null, cancellationToken: cancellationToken);
+                return;
+            }
+
+            // ensure we have full messages
+            if (!result.EndOfMessage)
+            {
+                logger.LogWarning("WebSocket Connection sent a partial message that we do not support. Closing");
+                await socket.CloseAsync(WebSocketCloseStatus.InvalidPayloadData, "The whole message must be sent once.", cancellationToken);
+                return;
+            }
+
+            // at this point we have a binary message
+
+            // Take only the data read and invoke the handler
+            var data = BinaryData.FromBytes(buffer[..result.Count]);
+            logger.LogDebug("Received message: {Data}", data);
+            var message = JsonSerializer.Deserialize(data, SC.Default.RealtimeConnectionIncomingMessage) ?? throw new InvalidOperationException("Unable to desrialize incoming message");
+            await handler(message, cancellationToken);
+        }
     }
 
     public void Dispose()
     {
-        client?.Dispose();
+        socket?.Dispose();
     }
 
-    private WebsocketClient GetClient()
+    private ClientWebSocket GetSocket()
     {
-        if (client is null)
+        if (socket is null)
         {
             throw new InvalidOperationException($"The websocket client has not been initialized. '{nameof(StartAsync)}(...)' must be called first.");
         }
 
-        return client;
+        return socket;
     }
 }
