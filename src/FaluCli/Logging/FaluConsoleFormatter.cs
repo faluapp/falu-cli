d437f6cca4d620941b4e6aa3aacda493c165e5ad
Maxwell Weru <mburumaxwell@gmail.com>
2024-06-07 08:55:50 +0300

    Remove console logger code that cannot be safely trimmed (#274)

--- src/FaluCli/Logging/FaluConsoleFormatter.cs
+++ src/FaluCli/Logging/FaluConsoleFormatter.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Options;
-
-#nullable disable
+using System.Runtime.CompilerServices;
 
 namespace Falu.Logging;
 
+// We have this because the SimpleConsoleFormatter does not offset follow up lines with the size of the time, log level, and its padding
 internal class FaluConsoleFormatter : ConsoleFormatter
 {
     private const string LogLevelPadding = ": ";
-    private readonly IDisposable _optionsReloadToken;
+    private readonly IDisposable? _optionsReloadToken;
 
     public FaluConsoleFormatter(IOptionsMonitor<FaluConsoleFormatterOptions> options) : base("falu")
     {
@@ -27,9 +27,9 @@ internal class FaluConsoleFormatter : ConsoleFormatter
         _optionsReloadToken?.Dispose();
     }
 
-    internal FaluConsoleFormatterOptions FormatterOptions { get; set; }
+    internal FaluConsoleFormatterOptions? FormatterOptions { get; set; }
 
-    public override void Write<TState>(in LogEntry<TState> logEntry, IExternalScopeProvider scopeProvider, TextWriter textWriter)
+    public override void Write<TState>(in LogEntry<TState> logEntry, IExternalScopeProvider? scopeProvider, TextWriter textWriter)
     {
         string message = logEntry.Formatter(logEntry.State, logEntry.Exception);
         if (logEntry.Exception == null && message == null)
@@ -40,8 +40,8 @@ internal class FaluConsoleFormatter : ConsoleFormatter
         ConsoleColors logLevelColors = GetLogLevelConsoleColors(logLevel);
         string logLevelString = GetLogLevelString(logLevel);
 
-        string timestamp = null;
-        string timestampFormat = FormatterOptions.TimestampFormat;
+        var timestamp = (string?)null;
+        var timestampFormat = FormatterOptions?.TimestampFormat;
         if (timestampFormat != null)
         {
             DateTimeOffset dateTimeOffset = GetCurrentDateTime();
@@ -53,18 +53,18 @@ internal class FaluConsoleFormatter : ConsoleFormatter
         }
         if (logLevelString != null)
         {
-            textWriter.WriteColoredMessage(logLevelString, logLevelColors.Background, logLevelColors.Foreground);
+            WriteColoredMessage(textWriter, logLevelString, logLevelColors.Background, logLevelColors.Foreground);
         }
         CreateDefaultLogMessage(textWriter, logEntry, message, scopeProvider);
     }
 
-    private void CreateDefaultLogMessage<TState>(TextWriter textWriter, in LogEntry<TState> logEntry, string message, IExternalScopeProvider scopeProvider)
+    private void CreateDefaultLogMessage<TState>(TextWriter textWriter, in LogEntry<TState> logEntry, string message, IExternalScopeProvider? scopeProvider)
     {
-        bool singleLine = FormatterOptions.SingleLine;
-        bool includeCategory = FormatterOptions.IncludeCategory;
-        bool includeEventId = FormatterOptions.IncludeEventId;
+        bool singleLine = FormatterOptions?.SingleLine ?? false;
+        bool includeCategory = FormatterOptions?.IncludeCategory ?? false;
+        bool includeEventId = FormatterOptions?.IncludeEventId ?? false;
         int eventId = logEntry.EventId.Id;
-        Exception exception = logEntry.Exception;
+        var exception = logEntry.Exception;
 
         // Example:
         // info: ConsoleApp.Program[10]
@@ -80,12 +80,10 @@ internal class FaluConsoleFormatter : ConsoleFormatter
             {
                 textWriter.Write('[');
 
-#if NETCOREAPP
                 Span<char> span = stackalloc char[10];
                 if (eventId.TryFormat(span, out int charsWritten))
                     textWriter.Write(span[..charsWritten]);
                 else
-#endif
                     textWriter.Write(eventId.ToString());
 
                 textWriter.Write(']');
@@ -141,7 +139,7 @@ internal class FaluConsoleFormatter : ConsoleFormatter
 
     private DateTimeOffset GetCurrentDateTime()
     {
-        return FormatterOptions.UseUtcTimestamp ? DateTimeOffset.UtcNow : DateTimeOffset.Now;
+        return FormatterOptions?.UseUtcTimestamp ?? false ? DateTimeOffset.UtcNow : DateTimeOffset.Now;
     }
 
     private static string GetLogLevelString(LogLevel logLevel)
@@ -160,8 +158,8 @@ internal class FaluConsoleFormatter : ConsoleFormatter
 
     private ConsoleColors GetLogLevelConsoleColors(LogLevel logLevel)
     {
-        bool disableColors = FormatterOptions.ColorBehavior == LoggerColorBehavior.Disabled ||
-            FormatterOptions.ColorBehavior == LoggerColorBehavior.Default && Console.IsOutputRedirected;
+        bool disableColors = FormatterOptions?.ColorBehavior == LoggerColorBehavior.Disabled ||
+            FormatterOptions?.ColorBehavior == LoggerColorBehavior.Default && Console.IsOutputRedirected;
         if (disableColors)
         {
             return new ConsoleColors(null, null);
@@ -180,12 +178,12 @@ internal class FaluConsoleFormatter : ConsoleFormatter
         };
     }
 
-    private void WriteScopeInformation(TextWriter textWriter, IExternalScopeProvider scopeProvider, bool singleLine, MessagePaddings paddings)
+    private void WriteScopeInformation(TextWriter textWriter, IExternalScopeProvider? scopeProvider, bool singleLine, MessagePaddings paddings)
     {
-        if (FormatterOptions.IncludeScopes && scopeProvider != null)
+        if (FormatterOptions?.IncludeScopes ?? false && scopeProvider != null)
         {
             bool paddingNeeded = !singleLine;
-            scopeProvider.ForEachScope((scope, state) =>
+            scopeProvider?.ForEachScope((scope, state) =>
             {
                 if (paddingNeeded)
                 {
@@ -219,10 +217,10 @@ internal class FaluConsoleFormatter : ConsoleFormatter
         public string MessagePadding { get; } = messagePadding;
         public string NewLineWithMessagePadding { get; } = newLineWithMessagePadding;
 
-        public static MessagePaddings Create(FaluConsoleFormatterOptions options)
+        public static MessagePaddings Create(FaluConsoleFormatterOptions? options)
         {
             string messagePadding, newLineWithMessagePadding;
-            var timestampFormat = options.TimestampFormat;
+            var timestampFormat = options?.TimestampFormat;
             if (timestampFormat is not null)
             {
                 messagePadding = new(' ', GetLogLevelString(LogLevel.Information).Length + LogLevelPadding.Length + timestampFormat.Length);
@@ -237,4 +235,224 @@ internal class FaluConsoleFormatter : ConsoleFormatter
             return new(messagePadding, newLineWithMessagePadding);
         }
     }
+
+    public static void WriteColoredMessage(TextWriter textWriter, string message, ConsoleColor? background, ConsoleColor? foreground)
+    {
+        // Order: backgroundcolor, foregroundcolor, Message, reset foregroundcolor, reset backgroundcolor
+        if (background.HasValue)
+        {
+            textWriter.Write(AnsiParser.GetBackgroundColorEscapeCode(background.Value));
+        }
+        if (foreground.HasValue)
+        {
+            textWriter.Write(AnsiParser.GetForegroundColorEscapeCode(foreground.Value));
+        }
+        textWriter.Write(message);
+        if (foreground.HasValue)
+        {
+            textWriter.Write(AnsiParser.DefaultForegroundColor); // reset to default foreground color
+        }
+        if (background.HasValue)
+        {
+            textWriter.Write(AnsiParser.DefaultBackgroundColor); // reset to the background color
+        }
+    }
+
+    internal sealed class AnsiParser(Action<string, int, int, ConsoleColor?, ConsoleColor?> onParseWrite)
+    {
+        /// <summary>
+        /// Parses a subset of display attributes
+        /// Set Display Attributes
+        /// Set Attribute Mode [{attr1};...;{attrn}m
+        /// Sets multiple display attribute settings. The following lists standard attributes that are getting parsed:
+        /// 1 Bright
+        /// Foreground Colours
+        /// 30 Black
+        /// 31 Red
+        /// 32 Green
+        /// 33 Yellow
+        /// 34 Blue
+        /// 35 Magenta
+        /// 36 Cyan
+        /// 37 White
+        /// Background Colours
+        /// 40 Black
+        /// 41 Red
+        /// 42 Green
+        /// 43 Yellow
+        /// 44 Blue
+        /// 45 Magenta
+        /// 46 Cyan
+        /// 47 White
+        /// </summary>
+        public void Parse(string message)
+        {
+            int startIndex = -1;
+            int length = 0;
+            int escapeCode;
+            ConsoleColor? foreground = null;
+            ConsoleColor? background = null;
+            var span = message.AsSpan();
+            const char EscapeChar = '\x1B';
+            ConsoleColor? color = null;
+            bool isBright = false;
+            for (int i = 0; i < span.Length; i++)
+            {
+                if (span[i] == EscapeChar && span.Length >= i + 4 && span[i + 1] == '[')
+                {
+                    if (span[i + 3] == 'm')
+                    {
+                        // Example: \x1B[1m
+                        if (IsDigit(span[i + 2]))
+                        {
+                            escapeCode = (int)(span[i + 2] - '0');
+                            if (startIndex != -1)
+                            {
+                                onParseWrite(message, startIndex, length, background, foreground);
+                                startIndex = -1;
+                                length = 0;
+                            }
+                            if (escapeCode == 1)
+                                isBright = true;
+                            i += 3;
+                            continue;
+                        }
+                    }
+                    else if (span.Length >= i + 5 && span[i + 4] == 'm')
+                    {
+                        // Example: \x1B[40m
+                        if (IsDigit(span[i + 2]) && IsDigit(span[i + 3]))
+                        {
+                            escapeCode = (int)(span[i + 2] - '0') * 10 + (int)(span[i + 3] - '0');
+                            if (startIndex != -1)
+                            {
+                                onParseWrite(message, startIndex, length, background, foreground);
+                                startIndex = -1;
+                                length = 0;
+                            }
+                            if (TryGetForegroundColor(escapeCode, isBright, out color))
+                            {
+                                foreground = color;
+                                isBright = false;
+                            }
+                            else if (TryGetBackgroundColor(escapeCode, out color))
+                            {
+                                background = color;
+                            }
+                            i += 4;
+                            continue;
+                        }
+                    }
+                }
+                if (startIndex == -1)
+                {
+                    startIndex = i;
+                }
+                int nextEscapeIndex = -1;
+                if (i < message.Length - 1)
+                {
+                    nextEscapeIndex = message.IndexOf(EscapeChar, i + 1);
+                }
+                if (nextEscapeIndex < 0)
+                {
+                    length = message.Length - startIndex;
+                    break;
+                }
+                length = nextEscapeIndex - startIndex;
+                i = nextEscapeIndex - 1;
+            }
+            if (startIndex != -1)
+            {
+                onParseWrite(message, startIndex, length, background, foreground);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IsDigit(char c) => (uint)(c - '0') <= ('9' - '0');
+
+        internal const string DefaultForegroundColor = "\x1B[39m\x1B[22m"; // reset to default foreground color
+        internal const string DefaultBackgroundColor = "\x1B[49m"; // reset to the background color
+
+        internal static string GetForegroundColorEscapeCode(ConsoleColor color)
+        {
+            return color switch
+            {
+                ConsoleColor.Black => "\x1B[30m",
+                ConsoleColor.DarkRed => "\x1B[31m",
+                ConsoleColor.DarkGreen => "\x1B[32m",
+                ConsoleColor.DarkYellow => "\x1B[33m",
+                ConsoleColor.DarkBlue => "\x1B[34m",
+                ConsoleColor.DarkMagenta => "\x1B[35m",
+                ConsoleColor.DarkCyan => "\x1B[36m",
+                ConsoleColor.Gray => "\x1B[37m",
+                ConsoleColor.Red => "\x1B[1m\x1B[31m",
+                ConsoleColor.Green => "\x1B[1m\x1B[32m",
+                ConsoleColor.Yellow => "\x1B[1m\x1B[33m",
+                ConsoleColor.Blue => "\x1B[1m\x1B[34m",
+                ConsoleColor.Magenta => "\x1B[1m\x1B[35m",
+                ConsoleColor.Cyan => "\x1B[1m\x1B[36m",
+                ConsoleColor.White => "\x1B[1m\x1B[37m",
+                _ => DefaultForegroundColor // default foreground color
+            };
+        }
+
+        internal static string GetBackgroundColorEscapeCode(ConsoleColor color)
+        {
+            return color switch
+            {
+                ConsoleColor.Black => "\x1B[40m",
+                ConsoleColor.DarkRed => "\x1B[41m",
+                ConsoleColor.DarkGreen => "\x1B[42m",
+                ConsoleColor.DarkYellow => "\x1B[43m",
+                ConsoleColor.DarkBlue => "\x1B[44m",
+                ConsoleColor.DarkMagenta => "\x1B[45m",
+                ConsoleColor.DarkCyan => "\x1B[46m",
+                ConsoleColor.Gray => "\x1B[47m",
+                _ => DefaultBackgroundColor // Use default background color
+            };
+        }
+
+        private static bool TryGetForegroundColor(int number, bool isBright, out ConsoleColor? color)
+        {
+            color = number switch
+            {
+                30 => ConsoleColor.Black,
+                31 => isBright ? ConsoleColor.Red : ConsoleColor.DarkRed,
+                32 => isBright ? ConsoleColor.Green : ConsoleColor.DarkGreen,
+                33 => isBright ? ConsoleColor.Yellow : ConsoleColor.DarkYellow,
+                34 => isBright ? ConsoleColor.Blue : ConsoleColor.DarkBlue,
+                35 => isBright ? ConsoleColor.Magenta : ConsoleColor.DarkMagenta,
+                36 => isBright ? ConsoleColor.Cyan : ConsoleColor.DarkCyan,
+                37 => isBright ? ConsoleColor.White : ConsoleColor.Gray,
+                _ => null
+            };
+            return color != null || number == 39;
+        }
+
+        private static bool TryGetBackgroundColor(int number, out ConsoleColor? color)
+        {
+            color = number switch
+            {
+                40 => ConsoleColor.Black,
+                41 => ConsoleColor.DarkRed,
+                42 => ConsoleColor.DarkGreen,
+                43 => ConsoleColor.DarkYellow,
+                44 => ConsoleColor.DarkBlue,
+                45 => ConsoleColor.DarkMagenta,
+                46 => ConsoleColor.DarkCyan,
+                47 => ConsoleColor.Gray,
+                _ => null
+            };
+            return color != null || number == 49;
+        }
+    }
+}
+
+internal class FaluConsoleFormatterOptions : SimpleConsoleFormatterOptions
+{
+    /// <summary>Includes category when true.</summary>
+    public bool IncludeCategory { get; set; } = true;
+
+    /// <summary>Includes event id when true.</summary>
+    public bool IncludeEventId { get; set; } = true;
 }
